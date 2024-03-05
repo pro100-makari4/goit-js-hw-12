aa589179cc0cf09d18ddc81482132a396297b6a5
Vladyslav Makarenko <makarenko.vlad.112@gmail.com>
2024-03-05 19:16:23 +0100

    touchscreen styles fixes

---
+++ src/js/touchscreen-checker.js
@@ -0,0 +1,5 @@
+let btn = document.querySelector('.rgb-style-btn');
+
+btn.addEventListener('touchend', event => {
+  event.preventDefault;
+});
--- src/main.js
+++ src/main.js
@@ -1,3 +1,4 @@
 import '/js/render-functions.js';
 import '/js/scroll-functions.js';
 import '/js/nightmode.js';
+import '/js/touchscreen-checker.js';
